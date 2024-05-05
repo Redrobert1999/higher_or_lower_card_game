a9c492bbd080a3a97e4f6dc978f1b02090fd0fc7
Robert Scott <robertscott58@hotmail.com>
2024-05-05 16:24:56 +0100

    Changed deck variable to list and added a method to retrieve the top card of the deck and put it into the discard variable.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 class CardDeck:
 
-    deck = {}
+    deck = []
+    discard = []
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
     faces = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
+    #List goes from [Top, ... , Bottom]
     for suit in suits:
-        card_value = 1
         for face in faces:
-            deck.update({face + " of " + suit: card_value})
-            card_value += 1
+            deck.append([face + " of " + suit])
+
+
+    def top_card(self):
+        top_card = self.deck[0]
+        self.discard.append(self.deck.pop(0))
+        return top_card
+
+
+
+deck_one = CardDeck()
+print("top_card =" + str(deck_one.top_card()))
+print("discard = " + str(deck_one.discard))
+print("deck = " + str(deck_one.deck))
+
 
 
 
