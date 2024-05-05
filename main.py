29c82d24df0fb5e837ebf407bbcd8ff6d692f18a
Robert Scott <robertscott58@hotmail.com>
2024-05-05 03:46:30 +0100

    Added CardDeck class and implemented deck variable to create new card decks.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-deck = {}
+class CardDeck:
 
-suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-faces = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    deck = {}
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    faces = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
-for suit in suits:
-    card_value = 1
-    for face in faces:
-        deck.update({face + " of " + suit: card_value})
-        card_value += 1
+    for suit in suits:
+        card_value = 1
+        for face in faces:
+            deck.update({face + " of " + suit: card_value})
+            card_value += 1
 
 
-print(deck)
+deck_one = CardDeck()
+print(deck_one.deck)
